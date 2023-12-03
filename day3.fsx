3cbc76f50e02273d79349606cc0c42aba6eb66c7
Jakub Majocha <majocha@outlook.com>
2023-12-03 09:08:01 +0100

    part two

--- day3.fsx
+++ day3.fsx
@@ -22,5 +22,29 @@ let numbersWithSymbols: int list =
             if not (Set.intersect symbolIndexes ext |> Set.isEmpty) then parse m.Value
     ]
 
-numbersWithSymbols |> sum
+let partOne = numbersWithSymbols |> sum
+
+let starSymbols = Regex.Matches(input, @"\*") |> Seq.map _.Index |> Set
+
+let adjMap =
+    [
+        for m in numbers do
+            let i, l = m.Index, m.Length
+            let ext = [i - 1 .. i + l] @  [i - 1 - D .. i + l - D] @ [i - 1 + D .. i + l + D]
+            for i in ext -> i, (parse m.Value : int)
+    ]
+    |> List.groupBy fst
+    |> List.filter (fun (i, vs) -> vs.Length = 2)
+    |> List.map (fun (i, vs) -> i, [ for _, v in vs -> v])
+    |> Map
+
+let matchingStars = Set.intersect (Set adjMap.Keys) starSymbols
+
+let partTwo =
+    [
+        for i in matchingStars do
+            let vs = adjMap[i]
+            vs[0] * vs[1]
+    ]
+    |> sum
 

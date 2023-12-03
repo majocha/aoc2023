f9efb1d6a7d2de4fa6376e3e7234a58eb64e296d
Jakub Majocha <majocha@outlook.com>
2023-12-03 08:45:21 +0100

    part one done

--- day3.fsx
+++ day3.fsx
@@ -7,16 +7,20 @@ open System.Text.RegularExpressions
 
 let input = System.IO.File.ReadAllLines "3.txt" |> String.concat ""
 
-let D = 10
+let D = 140
 
+let numbers = Regex.Matches(input, @"\d+") |> Seq.toList
+let symbols = Regex.Matches(input, @"[^\d|\.]") |> Seq.toList
 
-let numbers input =
-    [ for m in Regex.Matches(input, @"\d+") -> m.Value, m.Index]
+let symbolIndexes = symbols |> map _.Index |> Set
 
-let symbols input =
-    [ for m in Regex.Matches(input, @"[^\d|\.]") -> m.Value, m.Index]
+let numbersWithSymbols: int list =
+    [
+        for m in numbers do
+            let i, l = m.Index, m.Length
+            let ext = [i - 1 .. i + l] @  [i - 1 - D .. i + l - D] @ [i - 1 + D .. i + l + D] |> Set
+            if not (Set.intersect symbolIndexes ext |> Set.isEmpty) then parse m.Value
+    ]
 
+numbersWithSymbols |> sum
 
-numbers input
-
-symbols input

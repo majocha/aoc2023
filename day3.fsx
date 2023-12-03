79ea2f609faa617fa2957c40cdc8182b4476fd93
Jakub Majocha <majocha@outlook.com>
2023-12-03 11:54:54 +0100

    fantomas

--- day3.fsx
+++ day3.fsx
@@ -15,30 +15,25 @@ let symbols = Regex.Matches(input, @"[^\d|\.]") |> toList
 let symbolIndexes = symbols |> map _.Index |> Set
 
 let neighbourhood index length =
-    [index - 1 .. index + length] @
-    [index - 1 - D .. index + length - D] @
-    [index - 1 + D .. index + length + D]
+    [ index - 1 .. index + length ]
+    @ [ index - 1 - D .. index + length - D ]
+    @ [ index - 1 + D .. index + length + D ]
     |> Set
 
 
 
 let numbersWithSymbols: int list =
-    [
-        for m in numbers do
-            if not ( neighbourhood m.Index m.Length |> Set.intersect symbolIndexes |> Set.isEmpty) then
-                parse m.Value
-    ]
+    [ for m in numbers do
+          if not (neighbourhood m.Index m.Length |> Set.intersect symbolIndexes |> Set.isEmpty) then
+              parse m.Value ]
 
 let partOne = numbersWithSymbols |> sum
 
 let starSymbols = Regex.Matches(input, @"\*") |> Seq.map _.Index |> Set
 
 let adjMap =
-    [
-        for m in numbers do
-            for i in neighbourhood m.Index m.Length ->
-                i, (parse m.Value : int)
-    ]
+    [ for m in numbers do
+          for i in neighbourhood m.Index m.Length -> i, (parse m.Value: int) ]
     |> List.groupBy fst
     |> List.filter (fun (i, vs) -> vs.Length = 2)
     |> List.map (fun (i, vs) -> i, (snd vs[0], snd vs[1]))
@@ -47,10 +42,7 @@ let adjMap =
 let matchingStars = Set.intersect (Set adjMap.Keys) starSymbols
 
 let partTwo =
-    [
-        for i in matchingStars do
-            let v1, v2 = adjMap[i]
-            v1 * v2
-    ]
+    [ for i in matchingStars do
+          let v1, v2 = adjMap[i]
+          v1 * v2 ]
     |> sum
-

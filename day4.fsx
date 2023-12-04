349fd218b46e3ca2c1f7c16f0bf0c5c2f6ce60a6
Jakub Majocha <majocha@outlook.com>
2023-12-04 09:44:53 +0100

    day 4 part two

--- day4.fsx
+++ day4.fsx
@@ -1,5 +1,6 @@
 #time
 #r "nuget: FSharpPlus"
+
 open FSharpPlus
 open System.Text.RegularExpressions
 
@@ -7,20 +8,23 @@ let input = System.IO.File.ReadAllLines "4.txt"
 
 let cards =
     [ for line in input do
-        let i, s1,s2 = sscanf "Card %d: %s| %s" line
-        let winning = Regex.Matches (s1, "(\d+)") |> Seq.map _.Value |> map parse<int> |> Set
-        let your = Regex.Matches (s2, "(\d+)") |> Seq.map _.Value |> map parse<int> |> Set
-        i, winning, your ]
+          let i, s1, s2 = sscanf "Card %d: %s| %s" line
+          let winning = Regex.Matches(s1, "(\d+)") |> Seq.map _.Value |> map parse<int> |> Set
+          let your = Regex.Matches(s2, "(\d+)") |> Seq.map _.Value |> map parse<int> |> Set
+          i, winning, your ]
 
-let points =
+let matches =
     [ for i, winning, your in cards do
-        let matches = Set.intersect winning your |> length
-        pown 2 (matches - 1)
-    ]
+          Set.intersect winning your |> length ]
 
-let partOne = points |> sum
+let points n = pown 2 (n - 1)
 
+let partOne = matches |> map points |> sum
 
+let totalCards = Array.create cards.Length 1
 
+for i, score in matches |> List.indexed do
+    for j in i + 1 .. i + score do
+        totalCards[j] <- totalCards[j] + totalCards[i]
 
-
+let partTwo = sum totalCards

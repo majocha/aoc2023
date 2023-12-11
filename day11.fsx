e48f806849bae64043461d95f91ae7daab8b6c71
Jakub Majocha <majocha@outlook.com>
2023-12-11 10:46:42 +0100

    simpler

--- day11.fsx
+++ day11.fsx
@@ -26,14 +26,14 @@ let isLineEmpty =
     fun d n -> galaxies |> filter (fun g -> g |> dim d = n) = []
     |> memoizeN
 
-let distance' g1 g2 d =
+let distance' e g1 g2 d =
     let s1, s2 = dim d g1, dim d g2
     let s1, s2 = min s1 s2, max s1 s2
-    let emptyLines = [ s1..s2 ] |> List.filter (isLineEmpty d) |> length
-    int64 (999_999 * emptyLines + s2 - s1)
+    let emptyLines = [ s1..s2 ] |> filter (isLineEmpty d) |> length
+    int64 (e * emptyLines + s2 - s1)
 
-let distance (g1, g2) =
-    distance' g1 g2 Row + distance' g1 g2 Col
+let distance e (g1, g2) =
+    distance' e g1 g2 Row + distance' e g1 g2 Col
 
 let allPairs (gs: _ list) =
     [ let S = gs.Length - 1
@@ -42,4 +42,5 @@ let allPairs (gs: _ list) =
           for j in i + 1 .. S do
               gs[i], gs[j] ]
 
-galaxies |> allPairs |> map distance |> sum
+let partOne = galaxies |> allPairs |> map (distance 1) |> sum
+let partTwo = galaxies |> allPairs |> map (distance 999_999) |> sum

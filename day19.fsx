202cd5a603efecee14afa30435fecb1f03e901fd
Jakub Majocha <majocha@outlook.com>
2023-12-20 00:49:06 +0100

    part 2

--- day19.fsx
+++ day19.fsx
@@ -5,35 +5,43 @@ open FSharpPlus
 
 let input = System.IO.File.ReadAllLines "19.txt"
 
-type Part = { X: int; M: int; A: int; S: int }
+type Cat =
+    | X
+    | M
+    | A
+    | S
 
-type Cat = X | M | A | S
+type Part = Map<Cat, int>
 
-let getC (p: Part) = function X -> p.X | M -> p.M | A -> p.A| S -> p.S
+let getRating (p: Part) cat = p[cat]
 
-type Condition =
-    | Gt of int
-    | Lt of int
-
-type Rule = { Condition: Condition; Category: Cat;  Next: string }
-
-type Workflow = Workflow of Rule list * next: string
+type Rule =
+    | Gt of Cat * int * string
+    | Lt of Cat * int * string
+    | Next of string
 
 let (|GT|_|) = trySscanf "%s>%d:%s"
 let (|LT|_|) = trySscanf "%s<%d:%s"
 
-let (|Cat|) = function "x" -> X | "m" -> M | "a" -> A | "s" -> S | _ -> failwith "wrong category"
+let (|Cat|) =
+    function
+    | "x" -> X
+    | "m" -> M
+    | "a" -> A
+    | "s" -> S
+    | _ -> failwith "wrong category"
 
 let rec parseWorkflow wfs =
     let rec parseRules acc =
         function
-        | GT(Cat c, d, next) :: rest -> parseRules ({Condition = Gt d; Category = c; Next = next } :: acc) rest
-        | LT(Cat c, d, next) :: rest -> parseRules ({Condition = Lt d; Category = c; Next = next } :: acc) rest
-        | [ next ] -> Workflow(acc |> List.rev, next)
+        | GT(Cat c, d, next) :: rest -> parseRules (Gt(c, d, next) :: acc) rest
+        | LT(Cat c, d, next) :: rest -> parseRules (Lt(c, d, next) :: acc) rest
+        | [ next ] -> (Next next) :: acc |> List.rev
         | _ -> failwith "invalid rule list"
+
     function
     | "" :: rest -> wfs, rest
-    | line :: rest -> 
+    | line :: rest ->
         let name, str = sscanf "%s{%s}" line
         let wf = str.Split(',') |> List.ofSeq |> (parseRules [])
         parseWorkflow ((name, wf) :: wfs) rest
@@ -43,64 +51,66 @@ let parseWorkflows = parseWorkflow []
 
 let parsePart str =
     let x, m, a, s = sscanf "{x=%d,m=%d,a=%d,s=%d}" str
-    { X = x; M = m; A = a; S = s }
+    Part [ X, x; M, m; A, a; S, s ]
 
 let workflows, parts =
-    let workflows, input = input |> List.ofSeq |> parseWorkflows
-    Map workflows, input |> List.map parsePart
+    let workflows, rest = input |> List.ofSeq |> parseWorkflows
+    Map workflows, rest |> List.map parsePart
 
-module PartOne = 
-    let (|NextWf|_|) p (rule: Rule) =
-        match rule.Condition, rule.Next with 
-        | Gt d, w when (getC p rule.Category) > d -> Some w
-        | Lt d, w when (getC p rule.Category) < d -> Some w
-        | _ -> None
+module PartOne =
 
     let rec processRules part =
         function
-        | NextWf part w :: _ -> processWorkflow part w
+        | Gt(c, d, w) :: _ when (getRating part c) > d -> processWorkflow part w
+        | Lt(c, d, w) :: _ when (getRating part c) < d -> processWorkflow part w
+        | [ Next w ] -> processWorkflow part w
         | _ :: rules -> processRules part rules
-        | [] -> None
+        | [] -> failwith "invalid rule list"
 
     and processWorkflow part =
         function
-        | "A" -> Some true
-        | "R" -> Some false
-        | w ->
-            let (Workflow( rules, next)) = workflows[w]
-            match processRules part rules with
-            | None -> processWorkflow part next
-            | x -> x
+        | "A" -> true
+        | "R" -> false
+        | w -> processRules part workflows[w]
 
-    let isAccepted part = processWorkflow part "in" |> Option.get
+    let isAccepted part = processWorkflow part "in"
 
-    let partOne = parts |> List.filter isAccepted |> List.sumBy (fun p -> p.X + p.M + p.A + p.S)
+    let partOne =
+        parts |> List.filter isAccepted |> List.sumBy (fun p -> p.Values |> Seq.sum)
 
 
-let filterByCondition = function Gt d -> (<) d | Lt d -> (>) d
+let filterByCondition k =
+    function
+    | Gt(c, d, _) when c = k -> (<) d
+    | Lt(c, d, _) when c = k -> (>) d
+    | _ -> fun _ -> true
 
-let combinations m = m |> Map.map (fun _ s -> s |> Set.count |> int64) |> Map.values |> Seq.reduce ((*))
+let combinations m =
+    m
+    |> Map.map (fun _ s -> s |> Set.count |> int64)
+    |> Map.values
+    |> Seq.reduce ((*))
 
-let rec acceptedFromWorkflow from w allowed =
-    if allowed |> Map.values |> Seq.exists Set.isEmpty then 0L
-    else
-    match w with
-    | "A" -> combinations allowed
+let rec acceptedFromWorkflow ratings =
+    function
+    | "A" -> combinations ratings
     | "R" -> 0L
     | w ->
-        let (Workflow( rules, next)) = workflows[w]
-        let results, last =
-            rules |> List.mapFold(fun p r ->
-                let filtered = 
-                    p |> Map.map (fun k v -> if k = r.Category then v |> Set.filter (filterByCondition r.Condition) else v)
-                let rest = p |> Map.map (fun k v -> if k = r.Category then v |> Set.filter (filterByCondition r.Condition >> not) else v)
-                filtered |> acceptedFromWorkflow w r.Next, rest)
-                allowed
-        let results = (acceptedFromWorkflow w next last) :: results
-        results |> sum
+        workflows[w]
+        |> List.fold
+            (fun (p, count) r ->
+                match r with
+                | Gt(c, d, w)
+                | Lt(c, d, w) ->
+                    let filtered = p |> Map.add c (p[c] |> Set.filter (filterByCondition c r))
+                    let rest = p |> Map.add c (p[c] |> Set.filter (filterByCondition c r >> not))
+                    rest, count + acceptedFromWorkflow filtered w
+                | Next w -> p, count + acceptedFromWorkflow p w)
+            (ratings, 0L)
+        |> snd
 
-let countCombinations = acceptedFromWorkflow "" "in"
+let countCombinations possible = acceptedFromWorkflow possible "in"
 
-let possible = [X; M; A; S] |> List.map( fun c -> c, Set [1 .. 4000]) |> Map
+let possible = [ X; M; A; S ] |> List.map (fun c -> c, Set [ 1..4000 ]) |> Map
 
-let partTwo =countCombinations possible
+let partTwo = countCombinations possible

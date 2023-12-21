4a673bcaca616cc673f90c424bea37f83c1d958e
Jakub Majocha <majocha@outlook.com>
2023-12-21 20:55:32 +0100

    p1 better

--- day21.fsx
+++ day21.fsx
@@ -16,26 +16,31 @@ let start, map =
         | _ -> s, m )
         ((0,0), Set.empty)
 
-let neighbours (x, y) =
-    [
-        for p in  [ x - 1, y; x + 1, y; x, y - 1; x, y + 1] do
-            if map.Contains p then p ]
-            // if map.Contains ((size + x) % size, (size + y) % size) then x, y ]
+let gp (x, y) =
+    let wrap v = if v % size < 0 then v % size + size else v % size
+    map.Contains (wrap x, wrap y)
 
-let distances start =
-    let rec search n dist q =
+let neighbours (x, y) = [ x - 1, y; x + 1, y; x, y - 1; x, y + 1] |> List.filter gp
+
+let reachable steps =
+    let rec search dist q =
         match q with
         | [] -> dist
         | p :: rest ->
-            match dist |> Map.tryFind p |> Option.defaultValue 999 with
-            | d when n < d ->
-                search
-                    (n + 1)
-                    (dist |> Map.add p n)
-                    (q @ neighbours p)
-            | _ -> search n dist rest
-    search 0 Map.empty [ start ]
+            let current = (dist |> Map.find p) + 1
+            neighbours p
+            |> List.fold ( fun (dist, q) n ->
+                match dist |> Map.tryFind n |> Option.defaultValue 999999 with
+                | d when d > current ->
+                    dist |> Map.add n current,
+                    if current = steps then q else n :: q
+                | _ -> dist, q )
+                (dist, rest)
+            ||> search
 
-let ds = distances start
+    search (Map [start, 0]) [ start ]
+    |> Map.filter (fun _ d -> d % 2 = steps % 2 && d <= steps) |> Map.count
+
+let partOne = reachable 64
 
 

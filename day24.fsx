41f0f54132180d91b793d16395f3f228e628b42f
Jakub Majocha <majocha@outlook.com>
2023-12-28 01:08:13 +0100

    d24

--- day24.fsx
+++ day24.fsx
@@ -7,59 +7,95 @@ open FSharpPlus
 open MathNet.Numerics.LinearAlgebra
 
 let input = System.IO.File.ReadAllLines "24.txt"
-let d = 1f
+
 let parseLine line =
     let pstr, vstr = sscanf "%s @ %s" line
-    let parseVector str = str |> String.split [ ", "] |> List.ofSeq |> List.map float
+
+    let parseVector str =
+        str |> String.split [ ", " ] |> List.ofSeq |> List.map float
+
     parseVector pstr |> vector, parseVector vstr |> vector
 
-let segments = input |> Seq.map parseLine |> Seq.toList
+let p, v = input |> Seq.map parseLine |> Seq.toList |> List.unzip
 
-let p = segments |> List.map fst
-let v = segments |> List.map snd
-
-let p1, p2, p3 = p[1], p[2], p[3]
-let v1, v2, v3 = v[1], v[2], v[3]
-
-
-
-
-
-
-
-
-module PartOne = 
+module PartOne =
     open System.Numerics
-    let d1, d2 = 2f , 4f 
+
+    type V = MathNet.Numerics.LinearAlgebra.Vector<float>
 
     let intersection (x1, x2) (x3, x4) =
         let a = x2 - x1
         let b = x4 - x3
         let c = x3 - x1
-        let x = x1 + a * Vector3.Dot(Vector3.Cross(c, b), Vector3.Cross(a, b)) / Vector3.Cross(a, b).LengthSquared()
-        x
 
-    let twoPoints (x,v) = x, v + x
+        let x =
+            x1
+            + a * Vector3.Dot(Vector3.Cross(c, b), Vector3.Cross(a, b))
+              / Vector3.Cross(a, b).LengthSquared()
+
+        x |> tap (printfn "%A")
+
+    let zz (x: V) = Vector3(single x[0], single x[1], 0f)
+
+    let d = 100000000000000f
 
     let belongs (x: Vector3) ((x1: Vector3), (v1: Vector3)) ((x2: Vector3), (v2: Vector3)) =
-        ( (x.X < x1.X && v1.X < 0f) || (x.X > x1.X && v1.X > 0f)) &&
-        ( (x.X < x2.X && v2.X < 0f) || (x.X > x2.X && v2.X > 0f)) &&
-        x.X >= d1 && x.X <= d2 && x.Y >= d1 && x.Y <= d2
+        let d1, d2 = 2f * d, 4f * d
+
+        ((x.X < x1.X && v1.X < 0f) || (x.X > x1.X && v1.X > 0f))
+        && ((x.X < x2.X && v2.X < 0f) || (x.X > x2.X && v2.X > 0f))
+        && x.X >= d1
+        && x.X <= d2
+        && x.Y >= d1
+        && x.Y <= d2
 
     let allPairs =
-        [
-            for i in 0 .. segments.Length - 2 do
-                for j in i + 1 .. segments.Length - 1 do 
-                    segments[i], segments[j] ]
+        [ for i in 0 .. input.Length - 2 do
+              for j in i + 1 .. input.Length - 1 do
+                  (zz p[i], zz v[i]), (zz p[j], zz v[j]) ]
 
     let intersectsInside (s1, s2) =
-        let l1, l2 = twoPoints s1, twoPoints s2
-        let x = intersection l1 l2
-        belongs x s1 s2
+        let l (p: Vector3, v) =
+            let p = p / d
+            p, p + v
 
-    intersectsInside (segments[0], segments[1])
+        let x = intersection (l s1) (l s2)
+        belongs (x * d) s1 s2
 
     let partOne = allPairs |> List.filter intersectsInside |> List.length
 
+let p1, p2, p3 = p[0], p[1], p[2]
+let v1, v2, v3 = v[0], v[1], v[2]
 
+let cross (a: _ Vector) (b: _ Vector) =
+    let a1, a2, a3 = a[0], a[1], a[2]
+    let b1, b2, b3 = b[0], b[1], b[2]
+    vector
+      [ a2 * b3 - a3 * b2
+        a3 * b1 - a1 * b3
+        a1 * b2 - a2 * b1 ]
 
+let crossMatrix (x: _ Vector) =
+    let x1, x2, x3 = x[0], x[1], x[2]
+    matrix
+      [ [ 0.; -x3; x2 ]
+        [ x3; 0.; -x1 ]
+        [ -x2; x1; 0. ] ]
+
+let b =
+    vector
+      [ yield! (cross p2 v2) - (cross p1 v1)
+        yield! (cross p3 v3) - (cross p1 v1) ]
+
+let M =
+    matrix
+        [ yield!
+              (crossMatrix v1 - crossMatrix v2)
+                  .Append(crossMatrix p2 - crossMatrix p1)
+                  .EnumerateRows()
+          yield!
+              (crossMatrix v1 - crossMatrix v3)
+                  .Append(crossMatrix p3 - crossMatrix p1)
+                  .EnumerateRows() ]
+
+M.Solve b |> Seq.take 3 |> Seq.sum |> printfn "Part two: %.0f"

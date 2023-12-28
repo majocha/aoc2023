a53bba72d2cb31f7b3ee0a1aa0105a52b34925c4
Jakub Majocha <majocha@outlook.com>
2023-12-28 02:57:34 +0100

    d25

--- day25.fsx
+++ day25.fsx
@@ -5,36 +5,35 @@ open FSharpPlus
 
 let input = System.IO.File.ReadAllLines "25.txt"
 
-let V, E' = input |> Seq.fold(fun (vs, es) line ->
-    let (v :: ws) = line |> String.replace ": " " " |> String.split [" "] |> List.ofSeq
-    vs + Set (v :: ws) , es + Set [for w in ws -> v, w] ) (Set.empty, Set.empty)
+let V, E =
+    input
+    |> Seq.fold
+        (fun (vertices, edges) line ->
+            let xs = line.Replace(": ", " ").Split(' ')
+            vertices + Set xs, edges + Set [ for w in xs[1..] -> xs[0], w ])
+        (Set.empty, Set.empty)
 
-let E = E' |> List.ofSeq
 let rnd = System.Random()
 
-let rec runPartition () =
+let rec runPartition n =
     let ss subsets x = subsets |> Seq.find (Set.contains x)
+    let randomEdge () = E |> Seq.item (rnd.Next E.Count)
+
     let rec partition subsets =
         if subsets |> Set.count <= 2 then subsets else
-            let u, v = E[rnd.Next E.Length]
-            let s1, s2 = ss subsets v, ss subsets u
+            let u, v = randomEdge ()
+            let s1, s2 = ss subsets u, ss subsets v 
             if s1 <> s2 then
                 subsets |> Set.remove s1 |> Set.remove s2 |> Set.add (s1 + s2)
-            else 
-                subsets
+            else subsets
             |> partition
 
-    let parts = V |> Set.map Set.singleton |> partition
-    if E |> List.filter (fun (u, v) -> ss parts u <> ss parts v) |> List.length < 4 then parts
+    let subsets = V |> Set.map Set.singleton
+    let parts = partition subsets
+    if E |> Set.filter (fun (u, v) -> ss parts u <> ss parts v) |> Set.count < 4 then
+        parts |> Set.map Set.count
     else
-        runPartition()
+        parts |> Set.map Set.count |> printfn "retrying %d, %A" n
+        runPartition (n + 1)
 
-let partOne = runPartition() |> Set.map Set.count |> Seq.reduce ((*))
-
-
-
-
-
-
-
-
+let partOne = runPartition 1 |> Seq.reduce ((*))

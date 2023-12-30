d6ed6642e8391b099413cd4c4f20a45eee613e6e
Jakub Majocha <majocha@outlook.com>
2023-12-30 01:25:20 +0100

    d21

--- day21.fsx
+++ day21.fsx
@@ -5,7 +5,7 @@ open FSharpPlus
 
 let input = System.IO.File.ReadAllLines "21.txt"
 let size = (input |> Seq.length)
-let d = size / 2
+let halfSize = size / 2
 let start = 0, 0
 let map =
     ([ 0 .. size - 1 ], [ 0 .. size - 1 ])
@@ -13,78 +13,70 @@ let map =
     |> List.fold
         (fun m (x, y) ->
             match input[y][x] with
-            | '.' | 'S' -> m |> Set.add (x - d, y - d)
+            | '.' | 'S' -> m |> Set.add (x - halfSize, y - halfSize)
             | _ -> m)
         Set.empty
 
 let gp (x, y) =
     let wrap v =
-        if (v + d) % size < 0 then (v + d) % size + size - d else (v + d) % size
+        let v = v % size
+        if abs v > halfSize then v - (sign v) * size else v
 
     map.Contains(wrap x, wrap y)
 
 let neighbours (x, y) =
     [ x - 1, y; x + 1, y; x, y - 1; x, y + 1 ] |> List.filter gp
 
-let rec search stopCondition dist q =
+let rec search maxSteps dist q =
     match q with
     | [] -> dist
     | p :: rest ->
-        let current = (dist |> Map.find p) + 1
+        match (dist |> Map.find p) + 1 with
+        | steps when steps > maxSteps ->
+            search maxSteps dist rest
+        | steps ->
+            neighbours p
+            |> List.fold
+                (fun (dist, q) n ->
+                    match dist |> Map.tryFind n |> Option.defaultValue 999999 with
+                    | d when d > steps ->
+                        dist |> Map.add n steps, q @ [ n ]
+                    | _ -> dist, q)
+                (dist, rest)
+            ||> search maxSteps
 
-        neighbours p
-        |> List.fold
-            (fun (dist, q) n ->
-                match dist |> Map.tryFind n |> Option.defaultValue 999999 with
-                | d when d > current -> dist |> Map.add n current, (if stopCondition n then q else q @ [ n ])
-                | _ -> dist, q)
-            (dist, rest)
-        ||> search stopCondition
+let countInSlice result steps sx sy =
+    result
+    |> Map.filter (fun (x, y) d ->
+        x >= sx * size - halfSize && x <= sx * size + halfSize &&
+        y >= sy * size - halfSize && y <= sy * size + halfSize &&
+        d % 2 = steps % 2)
+    |> Seq.length
 
-let fourGrid (x, y) =
-    x < - 5 * size || y < - 5 * size || x > 4 * size - 1 || y > 4 * size - 1
+let steps = 5 * size + halfSize
+let fromStart steps = search steps (Map [ start, 0 ]) [ start ]
+let result = fromStart steps
 
+for y in -10 .. 10 do
+    for x in -10 .. 10 do
+        let c = countInSlice result steps x y
+        if c = 0 then
+            printf "     "
+        else
+            printf "%5d" c
+    printfn "\n"
 
-let printGrid charF d =
-    for y in -d .. d do
-        for x in -d .. d do
-            charF (x, y) |> printf "%c"
-        printfn ""
+[
+    for y in -5 .. 5 do
+        for x in -5 .. 5 do
+            countInSlice result steps x y
+] |> List.countBy id
 
+let w = 202300L
 
-printGrid (fun p -> if gp p then '.'  else '#') size
-
-
-
-let fromStart = search fourGrid (Map [ start, 0 ]) [ start ]
-
-let countReachable steps start =
-    fromStart
-    |> Map.filter (fun _ d -> d % 2 = steps % 2 && d <= steps)
-    |> Map.count
-
-let partOne = countReachable 64 start
-
-let slice sx sy =
-    [ for x in sx * size .. (sx + 1) * size - 1 do
-          for y in sy * size .. (sy + 1) * size - 1 do
-              x, y ]
-
-let oddInSlice sx sy map =
-    [ for p in slice sx sy do
-          if map |> Map.containsKey p && map[p] % 2 = 1 && map[p] <= 6 * 131 + 65 then
-              true ]
-    |> List.length
-
-for y in -7 .. 5 do
-    for x in -6 .. 5 do
-        printf "[ %2d, %2d %4d ] " x y (oddInSlice x y fromStart)
-
-    printfn ""
-
-
-size
-
-[ 262..392 ] |> List.length
-131 * 3
-262 / 131
+let partTwo =
+    5765L + 5755L + 5788L + 5732L +
+    w * (980L + 1000L + 995L + 1000L) +
+    (w - 1L) * (6676L + 6706L + 6709L + 6683L) +
+    (pown w 2) * 7770L +
+    (pown (w - 1L) 2) * 7627L
